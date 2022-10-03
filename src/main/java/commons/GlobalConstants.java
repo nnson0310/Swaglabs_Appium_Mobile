67ef362eb7f6af151e454f3f87f7b5261ebe17a9
nnson0310 <nnson0310@gmail.com>
2022-10-03 15:17:36 +0700

    Add BasePage class

---
+++ src/main/java/commons/GlobalConstants.java
@@ -0,0 +1,6 @@
+package commons;
+
+public class GlobalConstants {
+    public static final long longTimeout = 20;
+    public static final long shortTimeout = 10;
+}
---
+++ src/main/java/helpers/MethodHelper.java
@@ -0,0 +1,12 @@
+package helpers;
+
+public class MethodHelper {
+
+    public static void sleepInSeconds(long seconds) {
+        try {
+            Thread.sleep(seconds * 1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
