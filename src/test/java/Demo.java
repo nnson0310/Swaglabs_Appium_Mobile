6678cd4f32b91af9dc9a32ff493750a383fbdfd1
nnson0310 <nnson0310@gmail.com>
2022-10-04 09:49:05 +0700

    Start Appium server programatically

--- src/test/java/Demo.java
+++ src/test/java/Demo.java
@@ -3,41 +3,44 @@ import commons.ServerManager;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
-import org.openqa.selenium.WebElement;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
-import java.net.URL;
 import java.time.Duration;
 
 public class Demo extends BaseTest {
 
     private static AppiumDriver driver;
+    private AppiumDriverLocalService server;
 
     @BeforeClass
-    public static void SetUp() throws MalformedURLException {
+    public void SetUp() throws MalformedURLException {
         UiAutomator2Options options = new UiAutomator2Options();
         options.setUdid("emulator-5554");
         options.setPlatformName("Android");
-        options.setAppActivity("com.swaglabsmobileapp");
+        options.setAppPackage("com.swaglabsmobileapp");
         options.setAppActivity("com.swaglabsmobileapp.MainActivity");
         options.setAutoGrantPermissions(true);
         options.setNoReset(true);
+        options.setAutomationName("UiAutomator2");
 
-        driver = new AndroidDriver(new ServerManager().getServer(), options);
+        server = new ServerManager().getServer();
+
+        driver = new AndroidDriver(server, options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
     }
 
     @Test
-    public static void Test() {
+    public void Test() {
         Assert.assertTrue(true);
     }
 
-    @AfterClass
-    public static void tearDown() {
+    @AfterClass(alwaysRun = true)
+    public void tearDown() {
         driver.quit();
     }
 }
